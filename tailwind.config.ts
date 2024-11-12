607628eb9e63ba2542e0b68381564b1b4eecc28b
Peter Phan <pet3r.ph4n@gmail.com>
2024-11-11 22:36:07 -0600

    Add new CSS variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,7 +27,10 @@ export default {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+			gridTemplateColumns: {
+				'classcell': '50% auto',
+			}
   	}
   },
   plugins: [require("tailwindcss-animate")],
