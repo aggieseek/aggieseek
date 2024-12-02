d17c32c73255238d91ad0299f34f49ae8c72270f
Peter Phan <pet3r.ph4n@gmail.com>
2024-12-02 01:53:56 -0600

    Remove unused types

--- lib/course-types.ts
+++ lib/course-types.ts
@@ -1,29 +1,3 @@
-export interface Instructor {
-  CV: string | null,
-  HAS_CV: "Y" | "N",
-  MORE: number,
-  NAME: string
-}
-
-export interface MeetingTime {
-  SSRMEET_BEGIN_TIME: string,
-  SSRMEET_BLDG_CODE: string,
-  SSRMEET_ROOM_CODE: string,
-  SSRMEET_CREDIT_HR_SESS: number,
-  SSRMEET_START_DATE: string,
-  SSRMEET_END_DATE: string,
-  SSRMEET_START_TIME: string,
-  SSRMEET_END_TIME: string,
-  SSRMEET_MTYP_CODE: string,
-  SSRMEET_SUN_DAY: string | null,
-  SSRMEET_MON_DAY: string | null,
-  SSRMEET_TUE_DAY: string | null,
-  SSRMEET_WED_DAY: string | null,
-  SSRMEET_THU_DAY: string | null,
-  SSRMEET_FRI_DAY: string | null,
-  SSRMEET_SAT_DAY: string | null,
-}
-
 export interface Attribute {
   SSRATTR_ATTR_CODE: string;
   STVATTR_DESC: string;
