b6822a44ffcce3e7a548b09dab7a155cc72de381
Sophia Phu <sophiatiffphu@gmail.com>
2024-12-13 21:37:21 -0600

    made aggieseek thingy smaller and altered padding

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -22,11 +22,11 @@ export default function Home() {
 
   return (
     <div className="">
-      <div className="flex justify-between mt-24 pl-24 gap-8">
+      <div className="flex mt-24 pl-8 lg:pl-24 gap-8">
 
         <div className="mt-10 mr-10">
-          <Image src={ "/images/logo-black.png" } alt={ "AggieSeek" } width={ 500 } height={ 200 }/>
-          <h2 className={ "font-bold mt-4 text-4xl text-neutral-500" }>Get your desired courses hassle free!</h2>
+          <Image src={ "/images/logo-black.png" } alt={ "AggieSeek" } width={ 400 } height={ 200 }/>
+          <h2 className={ "font-bold mt-4 text-3xl lg:text-4xl text-neutral-500" }>Get your desired courses hassle free!</h2>
 
           <div className={ "flex flex-col gap-y-2 text-sm my-8" }>
             <div className={ "flex gap-x-4" }>
@@ -57,7 +57,7 @@ export default function Home() {
                 </Link>
               </motion.div>
               : <Button onClick={ () => signIn('google', { callbackUrl: "/dashboard" }) }
-                        className={ "transition-transform p-1 pr-4 bg-blue-600 hover:bg-blue-700 active:scale-[0.97]" }>
+                        className={ "transition-transform p-1 pr-4 bg-[#502F2F] hover:bg-[#2d0908] active:scale-[0.97]" }>
                 <div
                   className={ "bg-white p-2 flex justify-center items-center text-black h-full aspect-square rounded-sm mr-2" }>
                   <Image className={ "w-auto h-auto" } src={ "/images/google-logo.png" } alt={ "Google" } width={ 50 }
@@ -68,7 +68,7 @@ export default function Home() {
         </div>
 
         <Image className={ "rounded-md shadow-xl" } src={ "/images/dashboard-preview.png" } alt={ "Dashboard" }
-               width={ 600 } height={ 300 }/>
+               width={ 700 } height={ 300 }/>
       </div>
 
     </div>
