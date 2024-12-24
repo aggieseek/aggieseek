9136fabcf422d0325c5e35b5e94b308d56f870c7
Sophia Phu <sophiatiffphu@gmail.com>
2024-12-24 03:34:20 -0600

    added footer to home page

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 export default function Layout({ children }: { children: ReactNode }) {
 
@@ -7,6 +8,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <div className={"font-[family-name:var(--font-geist-sans)]"}>
       <Navbar/>
       {children}
+      <Footer/>
     </div>
   );
 }
