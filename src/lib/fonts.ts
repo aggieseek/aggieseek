2774bf298aece341bd12881f9e3b4f5e6f80485f
Peter Phan <pet3r.ph4n@gmail.com>
2025-03-27 01:45:26 -0500

    Add new fonts

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,9 +1,24 @@
 import { Inter, JetBrains_Mono } from "next/font/google";
+import localFont from "next/font/local";
 
 export const inter = Inter({
-  subsets: ['latin']
+  subsets: ["latin"],
 });
 
 export const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
+});
+
+export const satoshi = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Satoshi-Variable.ttf",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Satoshi-VariableItalic.ttf",
+      style: "italic",
+    },
+  ],
+  variable: "--satoshi",
 });
