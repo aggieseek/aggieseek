ac2e90936a81f453318cafc3388b03d5020d9192
Peter Phan <pet3r.ph4n@gmail.com>
2025-03-30 21:08:01 -0500

    add additional metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,30 @@ import { ReactNode } from "react";
 import { satoshi } from "@/lib/fonts";
 import { Toaster } from "@/components/ui/sonner";
 
+const title = "AggieSeek";
+const description =
+  "Track courses at Texas A&M University and get notified when seats open. Add your courses and forget about them!";
 export const metadata: Metadata = {
-  title: "AggieSeek",
-  description:
-    "Track courses at Texas A&M University and get notified when seats open. Add your courses and forget about them!",
-  keywords: "aggieseek, aggie, seek, aggie seek, aggies",
+  title,
+  description,
+  keywords:
+    "aggieseek, aggie, seek, aggie seek, aggies, track, track course, track courses, track course availability, tracker, course tracker, aggie tracker, class tracker, tamu, tamu class tracker, tamu course, tamu course tracker",
+  openGraph: {
+    title,
+    description,
+    url: "https://aggieseek.net",
+    images: [
+      {
+        url: "/images/og-image.png",
+        secureUrl: "/images/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "AggieSeek Banner",
+      },
+    ],
+    type: "website",
+    siteName: "AggieSeek",
+  },
 };
 
 export default function RootLayout({
