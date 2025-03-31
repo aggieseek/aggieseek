1648741bf5b2212621628d8212deaf73aa1626b7
Sophia Phu <sophiatiffphu@gmail.com>
2025-03-30 22:53:36 -0500

    fixed changing widths

--- src/components/search-subject.tsx
+++ src/components/search-subject.tsx
@@ -41,13 +41,13 @@ export default function SearchSubject({
             variant="outline"
             role="combobox"
             aria-expanded={open}
-            className="w-full justify-between"
+            className="w-full justify-between lg:w-[200px]"
           >
             {selected ? selected : "Select subject..."}
             <ChevronsUpDown className="opacity-50" />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="p-0 w-[--radix-popover-trigger-width]">
+        <PopoverContent className="p-0 w-[--radix-popover-trigger-width] lg:w-[200px]">
           <Command>
             <CommandInput placeholder="Search subject..." className="h-9" />
             <CommandList>
